5c1fc74dc91adf1dc40109f95844abb5f8df58b9
root <root@lttwksp.lttwksp.mine>
2023-12-31 05:41:25 -0800

    add

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myblog',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '172.17.0.2',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'myblog',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': '172.17.0.2',
+    #     'PORT': '3306'
+    # }
 }
 
 
